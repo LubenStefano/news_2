3ed724d826f6195929d4e110f6415e9a9ddfc090
LubenStefano <lubenstefanovkn@gmail.com>
2025-01-30 22:21:17 +0200

    Remove unused PostFlowComponent import for cleaner app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./core/navbar/navbar.component";
 import { AdsAsideComponent } from './core/ads-aside/ads-aside.component';
 import { SideNavbarComponent } from "./core/side-navbar/side-navbar.component";
-import { PostFlowComponent } from "./post-flow/post-flow.component";
 import { LedDisplayComponent } from './led-display/led-display.component';
 import { TopCreatorsComponent } from "./top-creators/top-creators.component";
 
